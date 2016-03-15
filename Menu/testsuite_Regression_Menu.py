c1ec33f7d1221d4e404728c85d175ffcdbe546d6
jresmedina <jresmedina@gmail.com>
2016-03-15 17:09:26 +0800

    Initial Commit

---
+++ Menu/testsuite_Regression_Menu.py
@@ -0,0 +1,11 @@
+URL = "https://example.com"
+BASE_MENU_ELEMENT = "//div[@class='container container-menu']/div[2]/ul/li"
+
+HOME_ID = 1
+MENU01_ID = 2
+MENU02_ID = 3
+MENU03_ID = 4
+MENU04_ID = 5
+
+
+MENU_LIST = []
---
+++ ToDo/testsuite_ToDoModule.py
@@ -0,0 +1,11 @@
+#To do Label
+ALL_TEXT = "All"
+ACTIVE_TEXT = "Active"
+COMPLETED_TEXT = "Completed"
+
+TODO_LABEL = [ALL_TEXT, ACTIVE_TEXT, COMPLETED_TEXT]
+
+
+TODO_ITEM1 = "Create Test Automation"
+TODO_ITEM2 = "Enhanced Test Automation"
+TODO = [TODO_ITEM1, TODO_ITEM2]
